2a6286c67ce5cf9f3219c28f2ff1d7b3f00c214a
sushil-patil <sushpatil669@gmail.com>
2019-03-10 14:49:43 +0530

    Cherry pick Demo

--- src/GITADD.java
+++ src/GITADD.java
@@ -4,7 +4,7 @@ public class GITADD {
 	public static void main(String[] args) {
 		System.out.println(" Hello local 1 delete");  
 		System.out.println(" Hello local 1 commit 1");  
-		System.out.println(" Hello local 1 commit 2");  
+		System.out.println(" Checrry pick 1");  
 
 	}
 }
