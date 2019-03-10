3143a6bb54168caaa48ccc57fdfaf28705f90ca8
sushil-patil <sushpatil669@gmail.com>
2019-03-10 14:08:08 +0530

    Modifie  Local 1 commit

--- src/GITADD.java
+++ src/GITADD.java
@@ -2,7 +2,7 @@
 public class GITADD {
 
 	public static void main(String[] args) {
-		System.out.println(" Hello local 1 ");  
+		System.out.println(" Hello local 1 delete");  
 		System.out.println(" Hello local 1 commit 1");  
 		System.out.println(" Hello local 1 commit 2");  
 
