3c1fdcc7f76584010e08428c96dd0b7ef96d4520
sushil-patil <sushpatil669@gmail.com>
2019-03-10 14:00:40 +0530

    second Local commit

--- src/GITADD.java
+++ src/GITADD.java
@@ -2,7 +2,8 @@
 public class GITADD {
 
 	public static void main(String[] args) {
-		System.out.println(" Hello local 1");  
-	}
+		System.out.println(" Hello local 1 ");  
+		System.out.println(" Hello local 1 commit 1");  
 
+	}
 }
